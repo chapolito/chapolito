f735de4eb6584b8f0f13d40b3c42d11c2c6125d9
chapolito <jessechapo@gmail.com>
2016-02-17 03:18:13 -0800

    Adds and Converts old blog posts to markdown.

--- config.rb
+++ config.rb
@@ -63,6 +63,8 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+set :partials_dir, 'partials'
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
