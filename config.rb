3d3c39bd692011ef10491bb5a067546616695c1b
chapolito <jessechapo@gmail.com>
2016-02-20 23:10:10 -0800

    Adds Blog gem and formats blog articles

--- config.rb
+++ config.rb
@@ -25,12 +25,24 @@ end
 #   page "/admin/*"
 # end
 
-page '/free-design-resources/posts/*', :layout => :post_layout
+#page '/free-design-resources/posts/*', :layout => :post_layout
 
 
 # Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
+
+
+###
+# Blog
+###
+
+activate :blog do |blog|
+  blog.name = 'free-design-resources'
+  blog.prefix = 'free-design-resources'
+  blog.layout = 'post_layout'
+end
+
 
 ###
 # Helpers
