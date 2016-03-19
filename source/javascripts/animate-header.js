36b27c7cdc1cf847017b0a51296721b90da6576a
chapolito <jessechapo@gmail.com>
2016-03-19 10:52:40 -0700

    cleans up the header animations.

--- source/javascripts/animate-header.js
+++ source/javascripts/animate-header.js
@@ -4,7 +4,7 @@ $(function(){
   internal_nav = $('header a, footer nav a');
 
   internal_nav.click(function(e) {
-    header.addClass('drop-in-header');
+    header.addClass('animate-header-out');
   });
 
 });
